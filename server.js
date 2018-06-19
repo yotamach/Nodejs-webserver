4cf2e768ad0458a0c7f3385d5b34b64e0b5b6f2d
Yotam Achrak <yotam2585@gmail.com>
2018-06-19 15:03:03 +0300

    setup start script and env port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -43,8 +44,8 @@ app.get('/',(req,res) => {
     });
 });
 
-app.listen(3000,() => {
-    console.log('Server is up on port 3000');
+app.listen(port,() => {
+    console.log(`Server is up on port ${port}`);
 });
 
 app.get('/about',(req,res) => {
